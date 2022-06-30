838b0a636aac60b490645d540c188b5d88915b1d
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-06-30 16:08:20 -0300

    setting user controller

---
+++ src/controller/userController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+class UserController {
+
+  async findAll(req : Request, res : Response) {}
+  async findOne(req : Request, res : Response) {}
+  async create(req : Request, res : Response) {}
+  async update(req : Request, res : Response) {}
+  async destroy(req : Request, res : Response) {}
+
+}
+
+export default new UserController()
