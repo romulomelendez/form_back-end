2a2bef2bf4c2f793b83039d160b965b8c37f8344
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-06-30 16:04:30 -0300

    server file update

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express, { json } from 'express'
+import { database } from './database/database'
+import { router } from './route'
 
 const app = express()
 
 app.use(json())
+app.use(router)
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`App's running on port ${port}`))
+app.listen(port, async () => {
+  await database.sync()
+  console.log(`App's running on port ${port}`)
+})
