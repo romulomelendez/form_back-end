84c72848cae6e1c3d03c82c87f9aa782cda9b4df
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-06-30 16:00:43 -0300

    setting the route file

---
+++ src/route.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+export const router = express.Router()
+
+router.get('/users', )
+router.get('/users/:userId', )
+router.post('/users', )
+router.put('/users/:userId', )
+router.delete('/users/:userId', )
